d0bcb7d1ede1d5981bcbdcd0a63377b1e652fb39
Aral Atpulat <aralatpulat@aral-mac-mini.local>
2019-02-10 22:05:33 +0300

    comments deleted

--- CoinList/Common/UILabelExtensions.swift
+++ CoinList/Common/UILabelExtensions.swift
@@ -13,10 +13,8 @@ extension UILabel {
     func setRate(with rate: Float) {
         self.text = String(format: "%.1f", rate) + "%"
         if rate > 0 {
-            //return self.textColor = UIColor.green
             return self.textColor = UIColor(red: 0/255, green: 158/255, blue: 115/255, alpha: 1)
         } else {
-            //return self.textColor = UIColor.red
             return self.textColor = UIColor(red: 217/255, green: 64/255, blue: 64/255, alpha: 1)
         }
     }
