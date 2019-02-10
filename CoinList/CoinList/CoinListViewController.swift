fc35f6bb19846a1ce733ea56143a9f30b2bce8ec
Aral Atpulat <aralatpulat@aral-mac-mini.local>
2019-02-10 22:07:01 +0300

    coordinator added

--- CoinList/CoinList/CoinListViewController.swift
+++ CoinList/CoinList/CoinListViewController.swift
@@ -1,6 +1,6 @@
 //
 //  ViewController.swift
-//  CryptoList
+//  CoinList
 //
 //  Created by Aral Atpulat on 26.01.2019.
 //  Copyright © 2019 Aral Atpulat. All rights reserved.
@@ -8,26 +8,28 @@
 
 import UIKit
 
-class CryptoListViewController: UIViewController {
+class CoinListViewController: UIViewController {
     
-    @IBOutlet weak var cryptoTableView: UITableView!
+    @IBOutlet weak var coinTableView: UITableView!
     var coins: [Coin] = []
-    var viewModel: CryptoListViewModel!
+    var viewModel: CoinListViewModel!
+    var coordinator: Coordinator?
     
-    convenience init(viewModel: CryptoListViewModel) {
+    convenience init(viewModel: CoinListViewModel) {
         self.init()
         self.viewModel = viewModel
+        self.coordinator = Coordinator()
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        title = "Crypto List"
+        title = "Coin List"
         registerCell()
         apiCall()
     }
     
     private func registerCell(){
-        self.cryptoTableView.register(UINib(nibName: "CoinCell", bundle: nil), forCellReuseIdentifier: "CoinCell")
+        self.coinTableView.register(UINib(nibName: "CoinCell", bundle: nil), forCellReuseIdentifier: "CoinCell")
     }
     
     func apiCall(){
@@ -35,14 +37,14 @@ class CryptoListViewController: UIViewController {
         viewModel.getCoins{ [weak self] (coins) in
             self?.coins = coins
             DispatchQueue.main.async {
-                self?.cryptoTableView.reloadData()
+                self?.coinTableView.reloadData()
                 self?.removeLoading()
             }            
         }
     }
 }
 
-extension CryptoListViewController: UITableViewDataSource {
+extension CoinListViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return coins.count
     }
@@ -57,11 +59,9 @@ extension CryptoListViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {        
         let id = coins[indexPath.row].id
-        let coinDetailViewModel = CoinDetailViewModel()
-        let coinDetailViewController = CoinDetailViewController(viewModel: coinDetailViewModel, id: id)
-        self.show(coinDetailViewController, sender: nil)
+        coordinator?.openCoinDetail(of: id, vc: self)
     }
 }
 
-extension CryptoListViewController: UITableViewDelegate{}
+extension CoinListViewController: UITableViewDelegate{}
 
