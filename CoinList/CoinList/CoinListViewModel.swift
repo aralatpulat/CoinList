8d6b85a4f60fdef8f812aae6a0130c49f2b3df52
Aral Atpulat <aralatpulat@aral-mac-mini.local>
2019-03-11 00:04:59 +0300

    coin list added

--- CoinList/CoinList/CoinListViewModel.swift
+++ CoinList/CoinList/CoinListViewModel.swift
@@ -11,16 +11,33 @@ import Foundation
 class CoinListViewModel {
     
     let apiClient: APIClient
+    var coins: [Coin] = []
     
     init(_ apiClient: APIClient) {
         self.apiClient = apiClient
     }
     
-    func getCoins(completion: @escaping ([Coin]) -> Void ) {
-        apiClient.getCoinList{ (data, error) in
+    func getCoins(completion: @escaping () -> Void ) {
+        apiClient.getCoinList{ [weak self] (data, error) in
             if error == nil {
-                completion(data)
+                self?.coins = data
+                completion()
             }
         }
+    }
+    
+    func getCoins() -> [Coin] {
+        return coins
+    }
+    
+    func getCount() -> Int {
+        return coins.count
+    }
+    
+    func getCoin(index: Int) -> Coin? {
+        if index < coins.count {
+            return coins[index]
+        }
+        return nil
     }
 }
